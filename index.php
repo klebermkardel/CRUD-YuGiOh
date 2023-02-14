ca238628404f66c19daaa62f798378a1a349ea5a
klebermkardel <molinakardel@gmail.com>
2023-02-14 07:53:49 -0300

    Cria arquivo index.php para Tela de Login e estrutura de pastas com arquivos CSS e JavaScript

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yu-Gi-Oh! - Login</title>
+
+    <!-- CSS External Link -->
+    <link rel="stylesheet" href="assets/CSS/style.css">
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- JavaScript External Link -->
+    <script src="assets/JS/script.js"></script>
+</body>
+</html>
