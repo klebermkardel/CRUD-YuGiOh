9e6b86650e7ffa2eb784ba65f8d5f5e0d077e9b5
klebermkardel <molinakardel@gmail.com>
2023-02-14 08:25:36 -0300

    Cria arquivo cadastrar.php e cria e estiliza formulário de cadastro de novos usuários

---
+++ cadastrar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yu-Gi-Oh! - Novo Usuário</title>
+
+    <!-- CSS External Link -->
+    <link rel="stylesheet" href="assets/CSS/style.css">
+</head>
+<body>
+    <div id="corpo-form-cad">
+        <h1>NOVO USUÁRIO</h1>
+        <form action="" method="POST">
+            <input type="text" name="nome" placeholder="Nome Completo">
+            <input type="email" name="email" placeholder="E-mail">
+            <input type="password" name="senha" placeholder="Senha">
+            <input type="password" name="confSenha" placeholder="Confirmar Senha">
+            <input type="submit" value="CADASTRAR">
+            <a href="index.php"><small>Já possui cadastro? <strong>ENTRAR!</strong></small></a>
+        </form>
+    </div>
+    
